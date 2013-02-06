0a16237b994481278aa33a9a524cea3cc78decd5
Rob Fox <thefoxbrain@gmail.com>
2013-02-06 09:20:28 +0000

    weblog started

--- cms/settings.py
+++ cms/settings.py
@@ -122,6 +122,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admindocs',
 	'django.contrib.flatpages',
 	'search',
+	'coltrane',
 )
 
 # A sample logging configuration. The only tangible logging
---
+++ coltrane/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+from django.contrib.auth.models import User
+import datetime
+
+class Category(models.Model):
+	title = models.CharField(max_length=250, help_text='Maximum 250 characters.')
+	slug = models.SlugField(help_text="Suggested value automatically generated from title. Must be unique.")
+	description = models.TextField()
+
+	class Meta:
+		ordering = ['title']
+		verbose_name_plural = "Categories"
+
+	def __unicode__(self):
+		return self.title
+		
+	def get_absolute_url(self):
+		return "/categories/%s/" % self.slug
+		
+class Entry(models.Model):
+	LIVE_STATUS = 1
+	DRAFT_STATUS = 2
+	HIDDEN_STATUS = 3
+	STATUS_CHOICES = (
+		(LIVE_STATUS, 'Live'),
+		(DRAFT_STATUS, 'Draft'),
+		(HIDDEN_STATUS, 'Hidden'),
+	)
+	status = models.IntegerField(choices=STATUS_CHOICES, default=LIVE_STATUS)
+	title = models.CharField(max_length=250)
+	excerpt = models.TextField(blank=True)
+	body = models.TextField()
+	pub_date = models.DateTimeField(default=datetime.datetime.now)
+	models.SlugField(unique_for_date='pub_date')
+	author = models.ForeignKey(User)
+	enable_comments = models.BooleanField(default=True)
+	featured = models.BooleanField(default=False)
+	categories = models.ManyToManyField(Category)
